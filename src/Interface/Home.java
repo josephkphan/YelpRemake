e549e97d87e95d8e4afed8c088e7d633a1414c4f
Joey Phan <joseph.phan@whitehatsec.com>
2017-10-28 21:29:24 -0700

    added N/A as options to all dropdowns

--- src/Interface/Home.java
+++ src/Interface/Home.java
@@ -59,10 +59,10 @@ public class Home extends JFrame implements ActionListener {
      */
 
 
-    private String[] string_days_of_week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private String[] string_days_of_week = {"N/A","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
     private String[] string_hours_of_day =
-            {"12:00AM", "1:00AM", "2:00AM", "3:00AM", "4:00AM", "5:00AM", "6:00AM", "7:00AM", "8:00AM", "9:00AM", "10:00AM", "11:00AM",
+            {"N/A","12:00AM", "1:00AM", "2:00AM", "3:00AM", "4:00AM", "5:00AM", "6:00AM", "7:00AM", "8:00AM", "9:00AM", "10:00AM", "11:00AM",
                     "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM", "6:00PM", "7:00PM", "8:00PM", "9:00PM", "10:00PM", "11:00PM"
             };
 
