4cbdf19c272da839ed2836f9b4cd0c376d6b68c9
Joey Phan <joseph.phan@whitehatsec.com>
2017-11-11 23:40:41 -0800

    filtering by time works... LAST THING IS ATTRIBUTES

--- src/Home.java
+++ src/Home.java
@@ -60,8 +60,8 @@ public class Home extends JFrame implements ActionListener {
     private String[] string_days_of_week = {"N/A", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
     private String[] string_hours_of_day =
-            {"N/A", "12:00AM", "1:00AM", "2:00AM", "3:00AM", "4:00AM", "5:00AM", "6:00AM", "7:00AM", "8:00AM", "9:00AM", "10:00AM", "11:00AM",
-                    "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM", "6:00PM", "7:00PM", "8:00PM", "9:00PM", "10:00PM", "11:00PM"
+            {"N/A", "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "8:00", "09:00", "10:00", "11:00",
+                    "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"
             };
 
     private String[] main_business_categories = {
@@ -81,7 +81,11 @@ public class Home extends JFrame implements ActionListener {
 
     Object[][] data = new Object[][]{};
 
-    String[] data_ids;
+    ArrayList<String[]> data_arraylist = new ArrayList<>();
+
+    ArrayList<String> data_ids = new ArrayList<>();
+
+    ArrayList<String[]> schedule = new ArrayList<>();
 
 
     Object[][] reviews_data = new Object[][]{};
@@ -94,10 +98,10 @@ public class Home extends JFrame implements ActionListener {
 
     private Set<String> attributes_set = new HashSet<>();
 
-    private String day_of_week = "";
-    private String start_time = "";
-    private String end_time = "";
-    private String search_attribute = "";
+    private String day_of_week = "N/A";
+    private String start_time = "N/A";
+    private String end_time = "N/A";
+    private String search_attribute = "N/A";
     private StringBuilder business_id_requested = new StringBuilder();
     private StringBuilder review_business_name = new StringBuilder();
 
@@ -158,7 +162,7 @@ public class Home extends JFrame implements ActionListener {
                 queryFindBusiness.run();
                 scroll_panes.get("results").setVisible(false);
                 pane.remove(scroll_panes.get("results"));
-                scroll_panes.put("results", GeneralJStuff.createTableScrollPane(pane, result_columns, data,data_ids, 500, 50, 450, 400, business_id_requested,review_business_name, createReviews ));
+                scroll_panes.put("results", GeneralJStuff.createTableScrollPane(pane, result_columns, data, convertStringArrayListToArray(data_ids), 500, 50, 450, 400, business_id_requested, review_business_name, createReviews));
 
             }
         };
@@ -198,7 +202,12 @@ public class Home extends JFrame implements ActionListener {
             @Override
             public void run() {
                 System.out.println(drop_downs.get("end_time").getSelectedItem());
-                end_time = drop_downs.get("end_time").getSelectedItem().toString();
+                if(drop_downs.get("end_time").getSelectedItem().toString().compareTo("00:00")==0){
+                    end_time = "24:00";
+                }else {
+                    end_time = drop_downs.get("end_time").getSelectedItem().toString();
+                }
+
             }
         };
         Runnable r_attributes = new Runnable() {
@@ -222,7 +231,7 @@ public class Home extends JFrame implements ActionListener {
         scroll_panes.put("main_category", GeneralJStuff.createCheckBoxScrollPane(pane, main_business_categories, 50, 50, 145, 400, main_category_set, queryFindTypes));
         scroll_panes.put("sub_category", GeneralJStuff.createCheckBoxScrollPane(pane, sub_business_categories, 200, 50, 145, 400, subcategory_set, queryFindTypes));
         scroll_panes.put("attributes", GeneralJStuff.createCheckBoxScrollPane(pane, attributes, 350, 50, 145, 400, attributes_set, queryFindTypes));
-        scroll_panes.put("results", GeneralJStuff.createTableScrollPane(pane, result_columns, data,data_ids, 500, 50, 450, 400, business_id_requested,review_business_name, createReviews ));
+        scroll_panes.put("results", GeneralJStuff.createTableScrollPane(pane, result_columns, data, convertStringArrayListToArray(data_ids), 500, 50, 450, 400, business_id_requested, review_business_name, createReviews));
     }
 
 
@@ -255,58 +264,72 @@ public class Home extends JFrame implements ActionListener {
     Runnable queryFindBusiness = new Runnable() {
         @Override
         public void run() {
-            String search_query = "SELECT DISTINCT b.name, b.city, b.state, b.review_count, b.business_id " +
-                    "FROM Business b " +
-                    "INNER JOIN MainCategories mc ON b.business_id=mc.business_id " +
-                    "INNER JOIN SubCategories sc ON b.business_id=sc.business_id " +
-                    "INNER JOIN Attributes a ON b.business_id=a.business_id ";
+            if (main_category_set.size() == 0) {
+                System.out.println("Nothing Selected!");
+            } else {
+                String search_query = "SELECT DISTINCT b.name, b.city, b.state, b.review_count " +
+                        ", b.business_id, mon_open, mon_close, tue_open, tue_close, wed_open, wed_close " +
+                        ", thu_open, thu_close, fri_open, fri_close, sat_open, sat_close, sun_open, sun_close " +
+                        "FROM Business b " +
+                        "INNER JOIN MainCategories mc ON b.business_id=mc.business_id " +
+                        "INNER JOIN SubCategories sc ON b.business_id=sc.business_id " +
+                        "INNER JOIN Attributes a ON b.business_id=a.business_id ";
 
-            if (main_category_set.size() != 0) {
+                //if(main_category_set.size() != 0){
                 search_query += "WHERE ";
+                //}
+
+                // Adding Main Category Where Conditions
+                search_query += "( ";
+                search_query += createCategoriesString();
+                search_query += ") ";
+
+                // Adding Sub Categories Where Conditions
+                if (subcategory_set.size() > 0) {
+                    search_query += " AND (";
+                    search_query += createSubcategoriesString();
+                    search_query += ")";
+                }
+
+                // Adding Attributes Where Conditions
+                if (attributes_set.size() > 0) {
+                    search_query += " AND (";
+                    search_query += createAttributesString();
+                    search_query += ")";
+                }
+
+                // Querying Database
+                ArrayList<String[]> results = jdbc_handler.makeSearchQuery(search_query, 4);
+                System.out.println(Arrays.toString(data));
+                data = jdbc_handler.arrayListToObjectArray(results);
+                data_arraylist.clear();
+                data_arraylist = results;
+
+                // Does it again to get business IDs (used as reference for Reviews)
+                results = jdbc_handler.makeSearchQuery(search_query, 19);
+
+                data_ids.clear();
+                schedule.clear();
+                for (int i = 0; i < results.size(); i++) {
+                    data_ids.add(results.get(i)[4]);
+                    schedule.add(Arrays.copyOfRange(results.get(i), 5, 20));
+                }
+                System.out.println(data_ids.toString());
+                System.out.println("--------------------");
+                System.out.println(schedule.toString());
+
+                filterByTime();
+
             }
-
-            // Adding Main Category Where Conditions
-            search_query += "( ";
-            search_query += createCategoriesString();
-            search_query += ") ";
-
-            // Adding Sub Categories Where Conditions
-            if(subcategory_set.size() > 0) {
-                search_query += " AND (";
-                search_query += createSubcategoriesString();
-                search_query += ")";
-            }
-
-            // Adding Attributes Where Conditions
-            if(attributes_set.size() > 0) {
-                search_query += " AND (";
-                search_query += createAttributesString();
-                search_query += ")";
-            }
-
-            // Querying Database
-            ArrayList<String[]> results = jdbc_handler.makeSearchQuery(search_query, 4);
-            System.out.println(Arrays.toString(data));
-            data = jdbc_handler.arrayListToObjectArray(results);
-
-            // Does it again to get business IDs (used as reference for Reviews)
-            results = jdbc_handler.makeSearchQuery(search_query, 5);
-
-            data_ids = new String[results.size()];
-            for(int i=0; i<results.size(); i++){
-                data_ids[i] = results.get(i)[4];
-            }
-            System.out.println(Arrays.toString(data_ids));
         }
     };
-
 
 
     // Second Column: Based off of Category
     Runnable queryFindTypes = new Runnable() {
         @Override
         public void run() {
-            if(main_category_set.size() == 0){
+            if (main_category_set.size() == 0) {
                 attributes = new String[0];
                 sub_business_categories = new String[0];
 
@@ -318,7 +341,7 @@ public class Home extends JFrame implements ActionListener {
                 pane.remove(scroll_panes.get("attributes"));
                 scroll_panes.put("attributes", GeneralJStuff.createCheckBoxScrollPane(pane, attributes, 350, 50, 145, 400, attributes_set, queryFindTypes));
 
-            }else {
+            } else {
 
                 String search_query = "SELECT DISTINCT(sc.category) FROM SubCategories sc JOIN ";
                 search_query +=
@@ -356,14 +379,14 @@ public class Home extends JFrame implements ActionListener {
         @Override
         public void run() {
             String search_query = "SELECT DISTINCT(att.attribute)so FROM Attributes att JOIN ";
-            search_query+=
+            search_query +=
                     "( SELECT b.business_id as b_id, mc.category as mc_category, sc.category as sc_category " +
                             "FROM Business b " +
                             "INNER JOIN MainCategories mc ON b.business_id=mc.business_id " +
                             "INNER JOIN SubCategories sc ON b.business_id=sc.business_id ";
 
 
-            if (main_category_set.size() != 0){
+            if (main_category_set.size() != 0) {
                 search_query += "WHERE (";
             }
 
@@ -402,12 +425,11 @@ public class Home extends JFrame implements ActionListener {
     };
 
 
-
     // Second Column: Based off of Category
     Runnable createReviews = new Runnable() {
         @Override
         public void run() {
-            if (open_every_other_counter++ %2 == 0) {//TODO git staHOT FIX
+            if (open_every_other_counter++ % 2 == 0) {//TODO git staHOT FIX
                 String search_query = "SELECT r.date_string, r.stars, r.text, r.user_id, r.v_useful " +
                         "FROM Business b " +
                         "INNER JOIN Review r ON b.business_id=r.business_id " +
@@ -428,12 +450,12 @@ public class Home extends JFrame implements ActionListener {
 
     // ------------------------- Miscellaneous ----------------------- //
 
-    private String addQuotes(String s){
+    private String addQuotes(String s) {
         return "'" + (s) + "'";
     }
 
 
-    private String createCategoriesString(){
+    private String createCategoriesString() {
         String string = "";
         int counter = 0;
         for (String str : main_category_set) {
@@ -446,7 +468,7 @@ public class Home extends JFrame implements ActionListener {
         return string;
     }
 
-    private String createSubcategoriesString(){
+    private String createSubcategoriesString() {
         String string = "";
         int counter = 0;
         for (String str : subcategory_set) {
@@ -459,10 +481,10 @@ public class Home extends JFrame implements ActionListener {
         return string;
     }
 
-    private String createAttributesString(){
+    private String createAttributesString() {
         int counter = 0;
         String string = "";
-        for (String str : attributes_set){
+        for (String str : attributes_set) {
             string += " a.attribute=";
             string += addQuotes(str);
             if (counter++ != attributes_set.size() - 1) {
@@ -472,6 +494,124 @@ public class Home extends JFrame implements ActionListener {
         return string;
     }
 
+    private void filterByTime() {
+        /*schedule String array
+            0    mon_open VARCHAR(100),
+            1    mon_close VARCHAR(100),
+            2    tue_open VARCHAR(100),
+            3    tue_close VARCHAR(100),
+            4    wed_open VARCHAR(100),
+            5    wed_close VARCHAR(100),
+            6    thu_open VARCHAR(100),
+            7    thu_close VARCHAR(100),
+            8    fri_open VARCHAR(100),
+            9    fri_close VARCHAR(100),
+            10   sat_open VARCHAR(100),
+            11   sat_close VARCHAR(100),
+            12   sun_open VARCHAR(100),
+            13   sun_close VARCHAR (100)
+         */
+        System.out.println(day_of_week);
+        switch (day_of_week) {
+            case "Monday":
+                timeChecker(0, 1);
+                break;
+            case "Tuesday":
+                timeChecker(2, 3);
+                break;
+            case "Wednesday":
+                timeChecker(4, 5);
+                break;
+            case "Thursday":
+                timeChecker(6, 7);
+                break;
+            case "Friday":
+                timeChecker(8, 9);
+                break;
+            case "Saturday":
+                timeChecker(10, 11);
+                break;
+            case "Sunday":
+                timeChecker(12, 13);
+                break;
+        }
 
+    }
+
+
+    private void timeChecker(int open_index, int end_index) {
+        boolean passed_start_check, passed_end_check;
+        int s1, s2, e1, e2;
+        String[] arr;
+        for (int i = 0; i < schedule.size(); i++) {
+            arr = schedule.get(i);
+
+            System.out.print(arr[open_index] + "  " + arr[end_index]);
+
+            if (arr[end_index].compareTo("null") == 0 &&
+                    arr[open_index].compareTo("null") == 0){
+                passed_start_check = false;
+                passed_end_check = false;
+            }else {
+
+                if (start_time.compareTo("N/A") == 0) {
+                    passed_start_check = true;
+                } else {
+                    s1 = timeToIntConverstion(start_time);
+                    if (arr[open_index].compareTo("null") == 0) {
+                        passed_start_check = false;
+                    } else {
+                        s2 = timeToIntConverstion(arr[open_index]);
+                        passed_start_check = (s2 <= s1);
+                    }
+                }
+
+                if (end_time.compareTo("N/A") == 0) {
+                    passed_end_check = true;
+                } else {
+                    e1 = timeToIntConverstion(end_time);
+                    if (arr[end_index].compareTo("null") == 0) {
+                        passed_end_check = false;
+                    } else {
+                        if (arr[end_index].compareTo("00:00") == 0) {
+                            arr[end_index] = "24:00";
+                        }
+                        e2 = timeToIntConverstion(arr[end_index]);
+
+                        passed_end_check = (e2 >= e1);
+                    }
+                }
+            }
+
+            if (!(passed_start_check && passed_end_check)) {
+                schedule.remove(i);
+                data_ids.remove(i);
+                data_arraylist.remove(i);
+                if(schedule.size()<=0){
+                    break;
+                }
+                i--;
+                System.out.println("DELETING ENTRY");
+            }else{
+                System.out.println("KEEPING ENTRY");
+
+            }
+        }
+        data = jdbc_handler.arrayListToObjectArray(data_arraylist);
+
+    }
+
+    private int timeToIntConverstion(String s) {
+        s = s.replaceAll(":", "");
+        s = s.replaceAll("AM", "");
+        s = s.replaceAll("PM", "");
+        return Integer.parseInt(s);
+    }
+
+
+    private String[] convertStringArrayListToArray(ArrayList<String> al) {
+        String[] arr = new String[al.size()];
+        return al.toArray(arr);
+    }
 
 }
